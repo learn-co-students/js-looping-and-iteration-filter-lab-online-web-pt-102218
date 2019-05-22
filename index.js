bccfc8dbf3c1c53c0a9acf77f7d0e9a8f3d26330
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-05-22 13:55:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+
+function findMatching(drivers, name) {
+  return drivers.filter(function(driverName) {
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
+}
+
+function fuzzyMatch(drivers, name) {
+  return drivers.filter(function(driverName){
+    return driverName.slice(0, name.length) === name;
+  })
+}
+
+function matchName(drivers, name) {
+ return drivers.filter(function(driver){
+   return driver.name === name;
+ });
+}
